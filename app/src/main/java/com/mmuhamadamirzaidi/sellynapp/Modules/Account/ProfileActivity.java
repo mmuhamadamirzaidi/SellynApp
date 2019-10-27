299805670534c8212f7ef5fc7660430ec397a887
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-10-27 16:46:42 +0800

    Create Profile Activity

---
+++ app/src/main/java/com/mmuhamadamirzaidi/sellynapp/Modules/Account/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.mmuhamadamirzaidi.sellynapp.Modules.Account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mmuhamadamirzaidi.sellynapp.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
