f65286d93ae0314e769032ef5a00b2af8dbd7d65
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-10-28 21:02:56 +0800

    Retrieve User Information

--- app/src/main/java/com/mmuhamadamirzaidi/sellynapp/Modules/Account/ProfileActivity.java
+++ app/src/main/java/com/mmuhamadamirzaidi/sellynapp/Modules/Account/ProfileActivity.java
@@ -6,12 +6,19 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.mmuhamadamirzaidi.sellynapp.Common.Common;
 import com.mmuhamadamirzaidi.sellynapp.R;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
     ImageView back, edit;
+
+    ImageView profile_image;
+
+    TextView profile_full_name, profile_ic_number, profile_phone_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,12 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.profile_toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
+        profile_image = (ImageView) findViewById(R.id.profile_image);
+
+        profile_full_name = (TextView) findViewById(R.id.profile_full_name);
+        profile_ic_number = (TextView) findViewById(R.id.profile_ic_number);
+        profile_phone_number = (TextView) findViewById(R.id.profile_phone_number);
 
         back = findViewById(R.id.back);
         edit = findViewById(R.id.edit);
@@ -39,5 +52,13 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(menuIntent);
             }
         });
+
+        // Set user information
+        profile_full_name.setText(Common.currentUser.getUserName());
+        profile_ic_number.setText(Common.currentUser.getUserIdentityCard());
+        profile_phone_number.setText(Common.currentUser.getUserPhone());
+
+        // Set image
+        Picasso.with(getBaseContext()).load(Common.currentUser.getUserImage()).into(profile_image);
     }
 }
